307ada933dc5e1c4520f63b03e4e77acf5b2389c
Hp <tailorkamlesh744@gmail.com>
2023-05-08 13:55:05 +0530

    first commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,14 @@ function Cart() {
     const dispatch=useDispatch();
     const newValue=useSelector(state=>state.increment.newProducts)
     // console.log(newValue);
+
+    function calculateTotalPrice(newValue) {
+        let totalPrice=0;
+        newValue.map(item => {
+            totalPrice += item.price*item.quantity
+        });
+        return totalPrice;
+    }
     return (
         <div className="cart"> 
         <div className="cart-section1">Your Cart (4 items)</div>
@@ -26,28 +34,27 @@ function Cart() {
                             <div>{element.title}</div>
                         </div>
                      
-                        <div className="cart-section3B">{element.price}</div>
+                        <div className="cart-section3B">₹{element.price}</div>
                         
                         <div className="cart-section3C">
-                            <button>+</button>
+                            <span>+</span>
                             <span>{element.quantity}</span>
-                            <button>-</button>
+                            <span>-</span>
                         </div>
-                       
-                        <div className="cart-section3D">{element.price*element.quantity}</div>
-                        
+                        <div className="cart-section3D">₹{element.price*element.quantity}</div>   
                     </div>
                     )
                 })
             }
+        { 
             <div className="cart-section4">
                 <div className="cart-section4A">
                     <div>Subtotal:</div>
-                    <div>$1019.98</div>
+                    <div>₹{calculateTotalPrice(newValue)}</div>
                 </div>
                 <div className="cart-section4B">
                     <div>Sales Tax:</div>
-                    <div>$102</div>
+                    <div>₹102</div>
                 </div>
                 <div className="cart-section4C">
                     <div>Coupon Code:</div>
@@ -55,7 +62,7 @@ function Cart() {
                 </div>
                 <div className="cart-section4D">
                     <div>Grand total:</div>
-                    <div>$1121.98</div>
+                    <div>₹{calculateTotalPrice(newValue)+102}</div>
                 </div>
                 <div className="cart-section5D">
                     <div>congrats, you're eligible for Free Shipping</div>
@@ -64,7 +71,8 @@ function Cart() {
                 <div className="cart-section6D">
                     <button>Check out</button>
                 </div>
-            </div>        
+            </div>
+        }             
         </div>
     )
 }
